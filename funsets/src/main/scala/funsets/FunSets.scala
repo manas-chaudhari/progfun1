e640e45528cdc62be7b762c9f8ad8ec4820c9b38
Manas Chaudhari <chaudhari.manas@gmail.com>
2016-07-09 13:09:14 +0530

    Refactor: Move common sets to traits

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -26,13 +26,13 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = x => contains(s, x) || contains(t, x)
+  def union(s: Set, t: Set): Set = x => contains(s, x) || contains(t, x)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t, x)
+  def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t, x)
   
   /**
    * Returns the difference of the two given sets,
